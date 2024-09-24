71f4783cdc14e5cda71b144d8541498a6544ba55
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-09-24 17:20:05 -0400

    feat: add basic routing test for application

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("proto-pad run!")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html")
+        fmt.Fprintln(w, "<h1>Home</h1><p>Welcome to the home page!</p>")
+    })
+
+    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html")
+        fmt.Fprintln(w, "<h1>Test</h1><p>This is the test page!</p>")
+    })
+
+    fmt.Println("Server starting on PORT 8000")
+    http.ListenAndServe(":8000", nil)
 }
